3582961c05a615e760da50e4820cd4f31b14f951
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-10-10 11:02:45 +0200

    added firebaseconfig

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, LogBox } from 'react-native';
+//import react navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import the screens we want to navigate
+import Start from './components/Start';
+import Chat from './components/Chat';
+//import to initialize a connection for Firestore
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-export default function App() {
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
+//create the navigator
+const Stack = createNativeStackNavigator();
+
+//the App's main Chat component that renders the chat UI
+const App = ()=> {
+  // here ist the  web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA07IWNlGbybZzjunKeN7koUkZmATWHVRA",
+    authDomain: "chat-app-12fcf.firebaseapp.com",
+    projectId: "chat-app-12fcf",
+    storageBucket: "chat-app-12fcf.appspot.com",
+    messagingSenderId: "685558259750",
+    appId: "1:685558259750:web:917d75b4a96e4249d7c6e7"
+  };
+
+// Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+ // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Start" component={Start} options={{headerTitleAlign:'center'}}></Stack.Screen>
+        <Stack.Screen name="Chat"  options={{headerTitleAlign:'center'}}> 
+          {(props) => (<Chat db={db} {...props} /> )}
+        </Stack.Screen>
+      </Stack.Navigator>
+      
+    </NavigationContainer> 
   );
 }
 
@@ -18,3 +53,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+export default App;
