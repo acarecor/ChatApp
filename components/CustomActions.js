516554f823c284c610b95dc0a9aeed5db7a7e7a8
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-10-11 21:44:47 +0200

    formatting

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,125 +1,147 @@
 import { TouchableOpacity, StyleSheet, View, Text, Alert } from "react-native";
 //react-native-action-sheet comes with the gifted-chat packages
-import {useActionSheet} from '@expo/react-native-action-sheet';
-import * as ImagePicker from 'expo-image-picker';
-import * as Location from 'expo-location';
+import { useActionSheet } from "@expo/react-native-action-sheet";
+import * as ImagePicker from "expo-image-picker";
+import * as Location from "expo-location";
 import { uploadBytes, ref, getDownloadURL } from "firebase/storage";
 
-
 //props provides from GiftedChat
-const CustomActions = ({wrapperStyle, iconTextStyle, onSend, storage}) => {
-    // return a reference to Gifted Chat’s ActionSheet
-    const actionSheet = useActionSheet();
-    //display a menu  with define actions for the user
-    const onActionPress = () => {
-        const options = ['Choose From Library', 'Take a Picture', 'Send Location', 'Cancel'];
-        const cancelButtonIndex = options.length -1;
-        actionSheet.showActionSheetWithOptions(
-            {
-                options, 
-                cancelButtonIndex,
-            },
-            async(buttonIndex)=> {
-              switch(buttonIndex){
-                case 0:
-                    pickImage();
-                    return;
-                case 1:
-                    takePhoto();
-                    return;
-                case 2:
-                    getLocation();
-                default:
-            }},
-        );
-    };
-
-    const generateReference =(uri) =>{
-        const timeStamp =(new Date()).getTime();
-        const imageName = uri.split("/")[uri.split("/").length -1];
-        return `${userID}-${timeStamp}-${imageName}`;
+const CustomActions = ({
+  wrapperStyle,
+  iconTextStyle,
+  onSend,
+  storage,
+  userID,
+}) => {
+  // return a reference to Gifted Chat’s ActionSheet
+  const actionSheet = useActionSheet();
+  //display a menu  with define actions for the user
+  const onActionPress = () => {
+    const options = [
+      "Choose From Library",
+      "Take a Picture",
+      "Send Location",
+      "Cancel",
+    ];
+    const cancelButtonIndex = options.length - 1;
+    actionSheet.showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+      },
+      async (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0:
+            pickImage();
+            return;
+          case 1:
+            takePhoto();
+            return;
+          case 2:
+            getLocation();
+          default:
+        }
       }
-const uploadAndSendImage = async(imageURI) =>{
+    );
+  };
+
+  //picks a picture from devices's library
+  const pickImage = async () => {
+    let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) await uploadAndSendImage(result.assets[0].uri);
+      else Alert.alert("Permissions haven't been granted");
+    }
+  };
+  
+//allow a photo to be taken when the user gives permission
+  const takePhoto = async () => {
+    let permissions = await ImagePicker.requestCameraPermissionsAsync();
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchCameraAsync();
+      if (!result.canceled) await uploadAndSendImage(result.assets[0].uri);
+      else Alert.alert("Permissions haven't been granted");
+    }
+  };
+//to share the user's location via a map
+  const getLocation = async () => {
+    let permissions = await Location.requestForegroundPermissionsAsync();
+    if (permissions?.granted) {
+      const location = await Location.getCurrentPositionAsync({});
+      if (location) {
+        onSend({
+          location: {
+            location: location.coords.longitude,
+            latitude: location.coords.latitude,
+          },
+        });
+      } else Alert.alert("Error occurred while fetching location");
+    } else Alert.alert("Permissions to read location aren't granted");
+  };
+
+  //
+  const generateReference = (uri) => {
+    const timeStamp = new Date().getTime();
+    const imageName = uri.split("/")[uri.split("/").length - 1];
+    return `${userID}-${timeStamp}-${imageName}`;
+  };
+
+  const convertFileToBlob = async (uri) => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (error) {
+        reject(new Error("XHR request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send();
+    });
+  };
+
+  const uploadAndSendImage = async (imageURI) => {
     const uniqueRefString = generateReference(imageURI);
-    const response = await fetch(imageURI);
-    const blob = await response.blob();
+    const blob = await convertFileToBlob(imageURI);
     const newUploadRef = ref(storage, uniqueRefString);
     //upload an image file blob using the Firebase Storage method
     uploadBytes(newUploadRef, blob).then(async (snapshot) => {
-        const imageURL = await getDownloadURL(snapshot.ref)
-        onSend({image: imageURL})
-      });
-    }
+      const imageURL = await getDownloadURL(snapshot.ref);
+      onSend({ image: imageURL });
+    });
+  };
 
-//picks a picture from devices's library
-    const pickImage = async () => {
-        let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (permissions?.granted) {
-          let result = await ImagePicker.launchImageLibraryAsync();
-          if (!result.canceled) await uploadAndSendImage(result.assets[0].uri);
-           else Alert.alert ("Permissions haven't been granted");
-        }
-      }
-
-      const takePhoto = async () => {
-        let permissions = await ImagePicker.requestCameraPermissionsAsync();
-        if (permissions?.granted) {
-          let result = await ImagePicker.launchCameraAsync();
-          if (!result.canceled) await uploadAndSendImage(result.assets[0].uri);
-          else Alert.alert("Permissions haven't been granted");
-        }
-      }
-    
-      const getLocation = async () => {
-        let permissions = await Location.requestForegroundPermissionsAsync();
-    
-        if (permissions?.granted) {
-          const location = await Location.getCurrentPositionAsync({});
-          if(location){
-            onSend({
-                location:{
-                location:location.coords.longitude,
-                latitude: location.coords.latitude,
-            },
-        });
-           
-          }else Alert.alert('Error occurred while fetching location');
-        } else Alert.alert("Permissions to read location aren't granted");
-        
-      }
-
-
-return(
-    <TouchableOpacity
-    style={styles.container}
-    onPress={onActionPress}>
-        <View style={[styles.wrapper, wrapperStyle]}>
-            <Text style={[styles.iconText, iconTextStyle]}></Text>
-        </View>
+  return (
+    <TouchableOpacity style={styles.container} onPress={onActionPress}>
+      <View style={[styles.wrapper, wrapperStyle]}>
+        <Text style={[styles.iconText, iconTextStyle]}>+</Text>
+      </View>
     </TouchableOpacity>
-)
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        width: 26,
-        height: 26,
-        marginLeft: 10,
-        marginBottom: 10,
-      },
-      wrapper: {
-        borderRadius: 13,
-        borderColor: '#b2b2b2',
-        borderWidth: 2,
-        flex: 1,
-      },
-      iconText: {
-        color: '#b2b2b2',
-        fontWeight: 'bold',
-        fontSize: 10,
-        backgroundColor: 'transparent',
-        textAlign: 'center',
-      },
-  });
+  container: {
+    width: 26,
+    height: 26,
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  wrapper: {
+    borderRadius: 13,
+    borderColor: "#b2b2b2",
+    borderWidth: 2,
+    flex: 1,
+  },
+  iconText: {
+    color: "#b2b2b2",
+    fontWeight: "bold",
+    fontSize: 15,
+    backgroundColor: "transparent",
+    textAlign: "center",
+  },
+});
 
 export default CustomActions;
