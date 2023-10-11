843d588d1213ac3e4e92b77f848a73b5eaed0bd0
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-10-11 16:27:57 +0200

    refactoring the code

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -34,39 +34,39 @@ const CustomActions = ({wrapperStyle, iconTextStyle, onSend, storage}) => {
         );
     };
 
+    const generateReference =(uri) =>{
+        const timeStamp =(new Date()).getTime();
+        const imageName = uri.split("/")[uri.split("/").length -1];
+        return `${userID}-${timeStamp}-${imageName}`;
+      }
+const uploadAndSendImage = async(imageURI) =>{
+    const uniqueRefString = generateReference(imageURI);
+    const response = await fetch(imageURI);
+    const blob = await response.blob();
+    const newUploadRef = ref(storage, uniqueRefString);
+    //upload an image file blob using the Firebase Storage method
+    uploadBytes(newUploadRef, blob).then(async (snapshot) => {
+        const imageURL = await getDownloadURL(snapshot.ref)
+        onSend({image: imageURL})
+      });
+    }
+
 //picks a picture from devices's library
     const pickImage = async () => {
         let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
         if (permissions?.granted) {
           let result = await ImagePicker.launchImageLibraryAsync();
-          if (!result.canceled){
-            const imageURI = result.assets[0].uri;
-            const uniqueRefString = generateReference(imageURI);
-            const response = await fetch(imageURI);
-            const blob = await response.blob();
-            const newUploadRef = ref(storage, uniqueRefString);
-            //upload an image file blob using the Firebase Storage method
-            uploadBytes(newUploadRef, blob).then(async (snapshot) => {
-                console.log('File has been uploaded successfully');
-                const imageURL = await getDownloadURL(snapshot.ref)
-                onSend({image: imageURL})
-            });
-          } else Alert.alert ("Permissions haven't been granted");
+          if (!result.canceled) await uploadAndSendImage(result.assets[0].uri);
+           else Alert.alert ("Permissions haven't been granted");
         }
-      }
-      const generateReference =(uri) =>{
-        const timeStamp =(new Date()).getTime();
-        const imageName = uri.split("/")[uri.split("/").length -1];
-        return `${userID}-${timeStamp}-${imageName}`;
       }
 
       const takePhoto = async () => {
         let permissions = await ImagePicker.requestCameraPermissionsAsync();
         if (permissions?.granted) {
           let result = await ImagePicker.launchCameraAsync();
-          if (!result.canceled){
-            console.log('uploading and uploading the image occurs here');
-            }  else Alert.alert("Permissions haven't been granted");
+          if (!result.canceled) await uploadAndSendImage(result.assets[0].uri);
+          else Alert.alert("Permissions haven't been granted");
         }
       }
     
